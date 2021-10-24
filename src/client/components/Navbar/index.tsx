09bf1b0db7c7d5c76b857478ad353fd6bff73eab
Jim Burch <jim@Jims-MacBook-Pro.local>
2021-10-24 16:09:06 -0700

    navbar styling and placeholders

--- src/client/components/Navbar/index.tsx
+++ src/client/components/Navbar/index.tsx
@@ -2,16 +2,45 @@ import React from "react";
 import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles({
-  root: {},
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100%",
+    background: "black",
+  },
+  navContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    maxWidth: 800,
+    height: 50,
+  },
+  leftContainer: {},
+  rightContainer: {
+    display: "flex",
+    flexDirection: "row",
+  },
 });
 
 const Navbar: React.FC = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <div>Navbar</div>
-    </div>
+    <header className={classes.root}>
+      <nav className={classes.navContainer}>
+        <div className={classes.leftContainer}>[ Logo ]</div>
+        <div className={classes.rightContainer}>
+          <div>Work</div>
+          <div>About</div>
+          <div>Contact</div>
+        </div>
+      </nav>
+    </header>
   );
 };
 
