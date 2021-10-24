b373652744261c9f48de1d8926ce95d9375e6fef
Jim Burch <jim@Jims-MacBook-Pro.local>
2021-10-24 16:22:29 -0700

    styled footer

--- src/client/components/Footer/index.tsx
+++ src/client/components/Footer/index.tsx
@@ -2,7 +2,23 @@ import React from "react";
 import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles({
-  root: {},
+  root: {
+    display: "flex",
+    justifyContent: "center",
+    position: "fixed",
+    bottom: 0,
+    left: 0,
+    width: "100%",
+    background: "black",
+  },
+  footerContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    maxWidth: 800,
+    height: 50,
+  },
 });
 
 const Footer: React.FC = () => {
@@ -10,7 +26,7 @@ const Footer: React.FC = () => {
 
   return (
     <div className={classes.root}>
-      <div>Footer</div>
+      <div className={classes.footerContainer}>Footer</div>
     </div>
   );
 };
