3caca0e344a4fb5ab18b2d6f35705ac89f4a7ec7
Jim Burch <jrburch1@gmail.com>
2022-01-16 13:17:12 -0700

    install mdx and setup gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,38 @@ module.exports = {
       },
     },
 
+    // mdx
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    "gatsby-remark-images",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+        defaultLayouts: {
+          posts: require.resolve("./src/components/post-layout.js"),
+        },
+      },
+    },
+
     // PWA
     {
       resolve: "gatsby-plugin-manifest",
