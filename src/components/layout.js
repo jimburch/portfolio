513391ade669a5e412daa979e04833042aead8b1
Jim Burch <jrburch1@gmail.com>
2022-01-22 15:40:05 -0700

    mdx and changeblog setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 
-import Header from "./header";
+import Header from "./header.js";
+import Seo from "./seo.js";
+
 import "../styles/global.css";
 import { main } from "../styles/layout.module.css";
 
-const Layout = ({ children }) => {
+const Layout = ({
+  children,
+  title = false,
+  description = false,
+  image = false,
+  path = false,
+}) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -18,6 +26,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Seo title={title} description={description} image={image} path={path} />
       <Header siteTitle={data.site.siteMetadata?.title || "Jim Burch"} />
       <div>
         <main className={main}>{children}</main>
