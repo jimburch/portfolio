212bd85378a1f471cf1c86e8c12b2adac2457f60
Jim Burch <jrburch1@gmail.com>
2022-01-25 21:15:56 -0700

    completed first post plus some style tweaks

--- src/pages/changeblog.js
+++ src/pages/changeblog.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
-// import { StaticImage } from "gatsby-plugin-image";
 
 import Layout from "../components/layout.js";
 
@@ -29,10 +28,6 @@ const Changeblog = () => {
     <Layout title="Jim Burch | Changeblog">
       <h1>Changeblog</h1>
       <p>An in-depth journal on how this site changes and grows.</p>
-      {/* <StaticImage
-        src="../images/monument-valley.jpg"
-        alt="landscape view of monument valley utah"
-      /> */}
       {posts.map((post) => (
         <div className={article} key={post.id}>
           <Link to={post.slug}>{post.frontmatter.title}</Link>
