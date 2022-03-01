816ed7672da41884924cb9238ae2504c80241a2a
Jim Burch <49823725+JimBurch@users.noreply.github.com>
2022-02-28 22:18:53 -0700

    Blog post prerender (#10)
    * push for computer switch
    
    * added ext url to frontmatter and helmet
    
    * delete gatsby node js

--- src/components/post-layout.js
+++ src/components/post-layout.js
@@ -3,17 +3,16 @@ import { Link } from "gatsby";
 import Layout from "./layout.js";
 
 const PostLayout = ({ children, pageContext }) => {
-  const { title, description, date, featuredImage } = pageContext.frontmatter;
+  const { title, description, metaImageUrl } = pageContext.frontmatter;
 
   return (
     <Layout
       title={`${title} | Changeblog`}
       description={description}
-      image={featuredImage}
+      image={metaImageUrl}
       style={{ paddingBottom: 50 }}
     >
       <h1>{title}</h1>
-      <p>{date}</p>
       {children}
       <Link to="/changeblog">&larr; back to Changeblog</Link>
     </Layout>
