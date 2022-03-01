f94e230e537e07bd76eebbbb43a5f52899569023
Jim Burch <jrburch1@gmail.com>
2022-02-28 21:35:54 -0700

    push for computer switch

--- src/components/post-layout.js
+++ src/components/post-layout.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Link } from "gatsby";
+// import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage } from "gatsby-plugin-image";
 import Layout from "./layout.js";
 
 const PostLayout = ({ children, pageContext }) => {
-  const { title, description, date, featuredImage } = pageContext.frontmatter;
+  const { title, description, date, featuredImgUrl, featuredImgAlt } =
+    pageContext.frontmatter;
+
+  console.log("IMAGE: ", featuredImgUrl);
 
   return (
     <Layout
       title={`${title} | Changeblog`}
       description={description}
-      image={featuredImage}
+      image={featuredImgUrl}
       style={{ paddingBottom: 50 }}
     >
+      {/* <StaticImage src={featuredImage} alt={title} /> */}
+      <GatsbyImage src={featuredImgUrl} alt={featuredImgAlt} />
       <h1>{title}</h1>
       <p>{date}</p>
       {children}
