8f46c25feb01a34105296a5b8b4301d19baddf39
Jim Burch <jrburch1@gmail.com>
2022-03-24 00:07:12 -0700

    new home page component and content

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
+import { Box, Heading, Text, Icon } from "@chakra-ui/react";
+import { MdWavingHand } from "react-icons/md";
+
+import { root, hero, heroHeadings, truncated } from "./Home.module.css";
+
+export const Home = () => (
+  <div className={root}>
+    <div className={hero}>
+      <>
+        <StaticImage
+          src="../../images/memoji.png"
+          alt="Jim Burch memoji sitting behind a MacBook Pro"
+          height={350}
+        />
+      </>
+      <div className={heroHeadings}>
+        <Heading as="h1" fontSize="4xl">
+          <Text as="span">{"<"}</Text>
+          <Text as="span" color="atom.red1">
+            {"h1"}
+          </Text>
+          <Text as="span">{">"}</Text>
+          <Text as="span">
+            My Name is Jim <Icon as={MdWavingHand} />
+          </Text>
+          <Text as="span">{"</"}</Text>
+          <Text as="span" color="atom.red1">
+            {"h1"}
+          </Text>
+          <Text as="span">{">"}</Text>
+        </Heading>
+        <Heading as="h2" fontSize="3xl">
+          Software Engineer & Creative Pro
+        </Heading>
+      </div>
+    </div>
+    <Box
+      fontSize="xl"
+      background="atom.highlight"
+      color="atom.mono3"
+      className={truncated}
+    >
+      <Box style={{ display: "block", paddingRight: 10 }}>
+        <Text as="span" color="atom.mono1">
+          {"<"}
+        </Text>
+        <Text as="span" color="atom.orange2">
+          {"Portfolio"}
+        </Text>
+        <Text as="span" color="atom.mono1">
+          {" /> "}
+        </Text>
+      </Box>
+      <Text isTruncated>
+        Jim Burch, 3 days ago • set up chakra and refactored nav menu for mobile
+        view
+      </Text>
+    </Box>
+  </div>
+);
---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Seo } from "../components/Seo";
+import { Home } from "../components/Home";
+
+const IndexPage = () => (
+  <>
+    <Seo />
+    <Home />
+  </>
+);
+
+export default IndexPage;
