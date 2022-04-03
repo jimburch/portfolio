1f7c3785e4d456e97f98dcdb10c9ace25b795732
Jim Burch <jrburch1@gmail.com>
2022-04-02 19:03:05 -0700

    setup outline for work module

---
+++ src/components/Work/Work.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { root } from "./Work.module.css";
+
+export const Work = () => {
+  return (
+    <div className={root}>
+      <div>Work</div>
+    </div>
+  );
+};
