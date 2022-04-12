f3accff3b3cafc708af7af7c8b19d8ad4594e83e
Jim Burch <jrburch1@gmail.com>
2022-04-11 23:22:13 -0700

    contentful rich text and project component layout

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,15 +1,47 @@
 import React from "react";
+import {
+  Box,
+  Heading,
+  Text,
+  UnorderedList,
+  ListItem,
+  ListIcon,
+  Link,
+} from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
 import { renderRichText } from "gatsby-source-contentful/rich-text";
+import { BLOCKS } from "@contentful/rich-text-types";
 
-import { root } from "./Project.module.css";
+import { root, imageContainer, details } from "./Project.module.css";
 
 export const Project = ({ projectName, description, image, url }) => {
+  const options = {
+    renderNode: {
+      [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+      [BLOCKS.UL_LIST]: (node, children) => (
+        <UnorderedList>{children}</UnorderedList>
+      ),
+      [BLOCKS.LIST_ITEM]: (node, children) => <ListItem>{children}</ListItem>,
+    },
+  };
+
   return (
-    <div className={root}>
-      <div>{projectName}</div>
-      <div>{renderRichText(description)}</div>
-      <img src={image?.url} alt={image?.description} />
-      <a href={url}>Link</a>
-    </div>
+    <Box className={root}>
+      <img
+        className={imageContainer}
+        src={image?.url}
+        alt={image?.description}
+        loading="lazy"
+      />
+      <div className={details}>
+        <Heading as="h3" size="lg">
+          {projectName}
+        </Heading>
+        {renderRichText(description, options)}
+        <Link href={url} color="atom.cyan">
+          New Link
+        </Link>
+      </div>
+    </Box>
   );
 };
